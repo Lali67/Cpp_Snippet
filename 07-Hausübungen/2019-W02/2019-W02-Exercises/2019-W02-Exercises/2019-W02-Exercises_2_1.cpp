8df8db2ed50dff495b0bbe3bcadc710b8d96fa35
Szőke Lajos <lajos67.szoke@gmail.com>
2019-10-16 09:25:22 +0200

    2_1; 2_2 English

--- 07-Hausübungen/2019-W02/2019-W02-Exercises/2019-W02-Exercises/2019-W02-Exercises_2_1.cpp
+++ 07-Hausübungen/2019-W02/2019-W02-Exercises/2019-W02-Exercises/2019-W02-Exercises_2_1.cpp
@@ -2,8 +2,8 @@
 Schreiben Sie ein Programm, das zwei Zahlen m und n einliest und die n-te Ziffer der Zahl m ausgibt.
 Z.B.: m=1358 n=2 Ausgabe: 5 (Die Ziffern werden ausgehend vom Dezimalpunkt gezählt).
 
-Dániel Szõke (11913915)
-12.10.2019
+Dániel Szöke (11913915)
+16.10.2019
 
 */
 
@@ -15,9 +15,11 @@ using namespace std;
 
 int countDigit(long long m, const int n)
 {
+	//1. Check input parameters
 	assert(n > 0);				// n must be positive
 	assert(m>=pow(10,n-1));		//check whether m>=10^(n-1)
 
+	//2. Run subprogram
 	int count = 0, digit = -1;
 	while ((m != 0) && (count!=n) ) {
 		digit = m - (m / 10) * 10;	//Z.B:  7 = 187-(187/10)*10 = 187-(18)*10=187-180 
@@ -30,8 +32,15 @@ int countDigit(long long m, const int n)
 int main()
 {
 	cout << "------------Test------------" << endl;
-	cout << "m=8, n=1:        " << countDigit(8,1) <<endl;
-	cout << "m=1358, n=2:     " << countDigit(1358, 2) << endl;
-	cout << "m=11913915, n=6: " << countDigit(11913915, 6) << endl;
+	cout << "m=8, n=1:        " << countDigit(8,1) <<endl;	// 8
+	cout << "m=1358, n=2:     " << countDigit(1358, 2) << endl; //5
+	cout << "m=11913915, n=6: " << countDigit(11913915, 6) << endl; //9
 	cout << endl;
+
+	// Get from the data from user to function
+	long long m; int n;
+	cout << "Dieses Programm berechnet...\n";
+	cout << "Kérem m értékét: "; cin >> m;
+	cout << "Kérem n értékét: "; cin >> n;
+	cout << "Eredmény: " << countDigit(m, n) << endl;
 }
