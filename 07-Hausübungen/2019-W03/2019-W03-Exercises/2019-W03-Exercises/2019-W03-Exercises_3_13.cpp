e8fc5c8d314afb49603fb858d2457da4126019f7
Szőke Lajos <lajos67.szoke@gmail.com>
2019-10-28 13:41:13 +0100

    Change

---
+++ 07-Hausübungen/2019-W03/2019-W03-Exercises/2019-W03-Exercises/2019-W03-Exercises_3_13.cpp
@@ -0,0 +1,43 @@
+/* 2019-W02-Exercise_3_13.cpp :
+
+Exercise 3.13
+Eine natürliche Zahl heißt fast perfekt, wenn die Summe ihrer echten Teiler 
+(das sind alle Teiler, die kleiner als die Zahl selbst sind) um eins kleiner ist, als die Zahl. 
+Zum Beispiel ist 4 fast perfekt, da 4=1*2*2 und 1+2=3=4-1. 
+Ermitteln Sie alle fast perfekten Zahlen im Bereich 0 bis 10^7.
+
+Dániel Szöke (11913915)
+26.10.2019
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+bool IsFastPerfectNumber(unsigned long n) {
+	unsigned long divisor{ 1 }, sumDivisors{ 0 };
+	bool result{ false };
+
+	while (divisor < n ) {
+		if (n % divisor == 0) sumDivisors += divisor;
+		divisor++;
+	}
+	
+	if (n-1 == sumDivisors) result = true;
+	
+	return result;
+}
+
+int main()
+{
+	unsigned long n{1};
+	cout << "This program...\n";
+	while (n < 1e+7) {
+		if (IsFastPerfectNumber(n)) cout << n << " ";
+		n++;
+	}
+	cout << endl;
+
+	return 0;
+}
---
+++ 07-Hausübungen/2019-W03/2019-W03-Exercises/2019-W03-Exercises/2019-W03-Exercises_3_14.cpp
@@ -0,0 +1,45 @@
+/* 2019-W02-Exercise_3_14.cpp :
+
+Exercise 3.14
+  Schreiben Sie ein Programm, das eine ganze Zahl einliest und die Anzahl der Inversionen 
+  in der eingegebenen Zahl ausgibt. Als Inversionen für eine bestimmte Ziffer (Ausgangsziffer) 
+  der Zahl bezeichnen wir alle Ziffern, die in der Zahl nach der Ausgangsziffer stehen und 
+  kleiner sind als diese. 
+  Zum Beispiel:
+	Eingabe: 53278     Ausgabe: 3
+	Da 3 hinter 5 steht, 2 hinter 5 steht und 2 hinter 3 steht.
+
+Dániel Szöke (11913915)
+26.10.2019
+
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int CountInversion(unsigned long n) {
+	string numberString;
+	int result{ 0 };
+
+	numberString = std::to_string(n);
+	for(int i=0; i<numberString.length(); i++)	{	
+		for (int j = i; j > -1; j--) {
+			if (numberString[j] > numberString[i]) result++;
+		}
+	}
+	
+	return result;
+}
+
+int main()
+{
+	unsigned long n{1};
+
+	cout << "This program...\n";
+	cout << "Get an integer number (n>0): "; cin >> n;
+	cout << "Result :" << CountInversion(n) << endl;
+
+	return 0;
+}
