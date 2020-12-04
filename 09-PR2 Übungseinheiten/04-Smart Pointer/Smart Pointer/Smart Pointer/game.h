0fd1ea5abeff158d4a8bd206168376b4aa927695
Lajos Szoke <lali67.szoke@live.com>
2020-12-04 14:05:04 +0100

    Commit

--- 09-PR2 Übungseinheiten/04-Smart Pointer/Smart Pointer/Smart Pointer/game.h
+++ 09-PR2 Übungseinheiten/04-Smart Pointer/Smart Pointer/Smart Pointer/game.h
@@ -1,15 +1,28 @@
-#ifndef GAME_H
-#define GAME_H
-
-#include<string>
 #include<iostream>
 #include<memory>
 #include<vector>
 #include<map>
 
+class Game :public
+{
+private:
+    std::string name; 											//Name des Spiels.
+    std::shared_ptr<Player> host;		 						//Leiter des Spiels.
+    std::map<std::string, std::shared_ptr<Player>> players;	    //Map von teilnehmenden Player-Objekten
+public:
+    Game(std::string name, std::shared_ptr<Player> host);
+    std::string get_name() const;
+    bool is_allowed(int n) const;
+    bool remove_player(const GameKey& gk, std::shared_ptr<Player> p);
+    bool add_player(const GameKey& gk, std::shared_ptr<Player> p);
+    std::shared_ptr<Player> best_player();
+    std::size_t number_of_players() const;
+    std::shared_ptr<Player> play(std::size_t i);
+    virtual int change(bool) const = 0;
+    virtual ~Game() = default;
+
+    virtual std::ostream& print(std::ostream& o) const;
+    friend std::ostream& operator<< (std::ostream& o, const Game& g);
+}
 
 #endif
-class Game
-{
-
-}
