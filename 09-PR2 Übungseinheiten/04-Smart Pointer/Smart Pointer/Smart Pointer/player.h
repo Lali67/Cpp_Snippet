38fba490a6f951671fcce61fb507a8b84b7d0e40
Szőke Dániel <szdani132@hotmail.com>
2020-12-04 14:03:23 +0100

    player

--- 09-PR2 Übungseinheiten/04-Smart Pointer/Smart Pointer/Smart Pointer/player.h
+++ 09-PR2 Übungseinheiten/04-Smart Pointer/Smart Pointer/Smart Pointer/player.h
@@ -1,10 +1,39 @@
-#include "player.h"
-#include "game.h"
-#include "gamekey.h"
+#ifndef PLAYER_H
+#define PLAYER_H
+
 #include<string>
 #include<iostream>
 #include<memory>
-#include<algorithm>
 #include<vector>
 #include<map>
+
+
+class Game;
+
+enum class Mode { Ranked, Unranked };
+
+class Player : public enable_shared_from_this <Player>()
+{
+private:
+	std::string name;
+	int mmr; 
+	std::shared_ptr<Game> hosted_game; 
+	map<std::string, std::weak_ptr<Game>> games;
+
+	Player(std::string name, int mmr) {}
+
+	std::string get_name() const;
+	int get_mmr() const;
+	std::shared_ptr<Game>get_hosted_game() const;
+	void change_mmr(int n);
+	bool host_game(std::string s, Mode m);
+	bool join_game(std::shared_ptr<Game> g);
+	bool leave_game(std::shared_ptr<Game> g);
+	std::vector<std::weak_ptr<Player>>invite_players(const std::vector<std::weak_ptr<Player>>& v);
+	bool close_game();
+	std::ostream& print(std::ostream& o, const Player& p) const;
+
+	friend std::ostream& operator <<(std::ostream& out, const Player& p);
+};
+#endif
 
